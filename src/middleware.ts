bf3be394142733e43d76b19e31f274d21190f032
bisaso andrew <bandrew235@gmail.com>
2025-10-13 12:00:25 +0000

    still getting the same errors, why?  Server Error {name: "Error", envi

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,44 +3,27 @@ import type { NextRequest } from 'next/server'
 
 import { i18n } from '@/i18n.config'
 
-import { match as matchLocale } from '@formatjs/intl-localematcher'
-import Negotiator from 'negotiator'
-
-function getLocale(request: NextRequest): string | undefined {
-  const negotiatorHeaders: Record<string, string> = {}
-  request.headers.forEach((value, key) => (negotiatorHeaders[key] = value))
-
-  const locales: string[] = [...i18n.locales]
-  const languages = new Negotiator({ headers: negotiatorHeaders }).languages()
-
-  try {
-    const locale = matchLocale(languages, locales, i18n.defaultLocale)
-    return locale
-  } catch (error) {
-    console.error("Error matching locale:", error);
-    return i18n.defaultLocale
-  }
-}
-
 export function middleware(request: NextRequest) {
   const pathname = request.nextUrl.pathname
+
+  // Check if there is any supported locale in the pathname
   const pathnameIsMissingLocale = i18n.locales.every(
-    locale => !pathname.startsWith(`/${locale}/`) && pathname !== `/${locale}`
+    (locale) => !pathname.startsWith(`/${locale}/`) && pathname !== `/${locale}`
   )
 
   // Redirect if there is no locale
   if (pathnameIsMissingLocale) {
-    const locale = getLocale(request)
+    const locale = i18n.defaultLocale
+
+    // e.g. incoming request is /products
+    // The new URL is now /en/products
     return NextResponse.redirect(
-      new URL(
-        `/${locale}${pathname.startsWith('/') ? '' : '/'}${pathname}`,
-        request.url
-      )
+      new URL(`/${locale}${pathname}`, request.url)
     )
   }
 }
 
 export const config = {
   // Matcher ignoring `/_next/` and `/api/`
-  matcher: ['/((?!api|_next/static|_next/image|favicon.ico|site.webmanifest).*)']
+  matcher: ['/((?!api|_next/static|_next/image|favicon.ico|site.webmanifest).*)'],
 }
